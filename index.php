d0e8c0e7086ea005f9d76fa4e99020c0df89834c
UMBERCARIO89 <umbercario@gmail.com>
2017-11-04 16:12:32 -0600

    I changed shit. Talk about it here.

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
       <h1><a href="https://sablesable.cf">sablesable.cf</a> — omar fejzic's website</h1>
       <h2>i wish only to seed within the populace extreme doubt of my own existence.</h2>
       <p>hey there. thanks for visiting this site, i guess. i am a programmer living in the wonderful state of utah
-          and i also do other shit. see my <a href="resume.php">resume</a> for more details. i'm sure everything else
+          and i also do other shit. see my resume for more details. i'm sure everything else
           will be kinda boring, so that sucks.</p>
       <h3>how to reach me</h3>
       <ul>
-          <li>email — <a href="mailto:yellowberry@cocaine.ninja">yellowberry@cocaine.ninja</a></li>
+          <li>email — <a href="http://frrp.mem">urnan@gmai.com</a></li>
           <li>discord — YellowberryHN#0483</li>
           <li>skype — <a href="skype:zsoltisawesome?chat">zsoltisawesome</a></li>
           <li>twitter — <a href="https://twitter.com/fireberry1337">fireberry1337</a></li>
