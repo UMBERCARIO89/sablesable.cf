7ac8b8288dc4d297a6ce17ec57a885ec54a458c9
UMBERCARIO89 <umbercario@gmail.com>
2017-11-04 16:40:59 -0600

    Changed personal contact info, changed projects I endorse, changed stylesheet.

--- index.php
+++ index.php
@@ -10,26 +10,21 @@
     </head>
     <body>
       <h1><a href="https://sablesable.cf">sablesable.cf</a> — omar fejzic's website</h1>
-      <h2>i wish only to seed within the populace extreme doubt of my own existence.</h2>
-      <p>hey there. thanks for visiting this site, i guess. i am a programmer living in the wonderful state of utah
-          and i also do other shit. see my resume for more details. i'm sure everything else
-          will be kinda boring, so that sucks.</p>
+      <h2>i read, a lot. but i do nothing more than i read.</h2>
+      <p> to zsolt, i am making my own website, with blackjack and hookers, and i'm all out of hookers.
+          so wanna play a round of blackjack?</p>
       <h3>how to reach me</h3>
       <ul>
-          <li>email — <a href="http://frrp.mem">urnan@gmai.com</a></li>
-          <li>discord — YellowberryHN#0483</li>
-          <li>skype — <a href="skype:zsoltisawesome?chat">zsoltisawesome</a></li>
-          <li>twitter — <a href="https://twitter.com/fireberry1337">fireberry1337</a></li>
-          <li>youtube — <a href="https://youtube.com/user/zsoltisawesome">YellowberryHN</a></li>
-          <li>cellular — <a href="tel:+13852556738">+1 (385) 255-6738</a></li>
-          <li>irc — zsoltisawesome on <a href="irc://irc.freenode.net/zsoltisawesome">freenode</a></li>
+          <li>email — <a href="mailto:umbercario@gmail.com">umbercario@gmail.com</a></li>
+          <li>discord — head_ruby#5687</li>
+          <li>cellular — <a href="tel:+18014949478">+1 (801) 494-9478</a></li>
       </ul>
-      <h3>my projects</h3>
+      <h3>projects i openly endorse, and some i support</h3>
       <ul>
-          <li><a href="https://harpnetstudios.com">harpnet studios</a> — my game company</li>
+          <li><a href="https://harpnetstudios.com">harpnet studios</a> — zsolt's game company</li>
           <li><a href="http://ddddwasd.gq">ddddwasd</a> — file dropper</li>
-          <li><a href="https://yello.ooo/blog/">yello|blog</a> — my blog, obviously</li>
-          <li><a href="https://yello.ooo/music">yellotunes</a> — the music that i write</li>
+          <li><a href="http://parahumans.wordpress.com">worm</a> — a web serial</li>
+          <li><a href="http://twigserial.wordpress.com">twig</a> — also a webserial</li>
       </ul>
       <p>i will eventually put more stuff here, but for now this is all i will write.</p>
       <h5>full source available on <a href="https://github.com/umbercario89/sablesable.cf">github</a></h5>
